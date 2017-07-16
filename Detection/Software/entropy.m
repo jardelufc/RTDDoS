f2e9cd64bab2b58492aa887766515fea625b513b
Riccellisp <riccellisp456@gmail.com>
2017-07-16 01:20:54 -0300

    Adding entropy IP based functionDetection/Software/entropy.m

---
+++ Detection/Software/entropy.m
@@ -0,0 +1,10 @@
+% Designed to perform to detect malicious traffic running in network, this
+% metric takes into account the source IPs Reference: Real-time DDoS attack 
+% detection using FPGA,2017. Section 3 
+function E = entropy(d)
+	if ischar(d), d=abs(d); end;
+        [Y,I,J] = unique(d); 	
+	H = sparse(J,1,1);
+	p = full(H(H>0))/length(d);
+	E = -sum(p.*log2(p));
+end 
