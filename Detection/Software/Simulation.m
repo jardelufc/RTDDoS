9ef53bffe0c9a3dca939d9d41b4c0a07ba74354e
Riccellisp <riccellisp456@gmail.com>
2017-09-05 19:31:05 -0300

    Simulation DebugedDetection/Software/Simulation.m

--- Detection/Software/Simulation.m
+++ Detection/Software/Simulation.m
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
+% Enable Debug mode
+debug = 1;
+DestIP = 24;
+=======
 DestIP = 3;
+>>>>>>> 945c18613071dfe0fbd79c591b81f903cbcd4d40
 % windownSize = 5;
 threshold = 0.81;
 % [PktRate VarSourceIP EntropySourceIP]
@@ -15,16 +21,32 @@ datasetMtxDestIP = datasetMtx(idxDestIP,:);
 % step to divide window time
 step = 1;
 time = 0;
+<<<<<<< HEAD
+win = 0;
+% keyboard;
+for i = 1:length(idxDestIP)
+    %     Getting send time of pkt
+=======
 for i = 1:length(idxDestIP)
 %     Getting send time of pkt
+>>>>>>> 945c18613071dfe0fbd79c591b81f903cbcd4d40
     pktSendTIME = datasetMtxDestIP(i,24);
     pktDelayNODE = datasetMtxDestIP(i,18);
     pktDELAY = datasetMtxDestIP(i,23);
     pktReservedTIME = datasetMtxDestIP(i,25);
+<<<<<<< HEAD
+    %     time is the sum of all delays
+    time = time + pktSendTIME + pktDelayNODE + pktDELAY + pktReservedTIME;
+    %
+    if time >= 1000
+        %         keyboard
+        win = win + 1;
+=======
 %     time is the sum of all delays
     time = time + pktSendTIME + pktDelayNODE + pktDELAY + pktReservedTIME;
 %     
     if time >= 1000
+>>>>>>> 945c18613071dfe0fbd79c591b81f903cbcd4d40
         trafficPktRate = mean(datasetMtxDestIP(step:i,19));
         sourceIPs = datasetMtxDestIP(step:i,1);
         %         VarSourceIPs = 0;
@@ -34,6 +56,50 @@ for i = 1:length(idxDestIP)
                 aux = aux + 1;
             end
         end
+<<<<<<< HEAD
+        
+        if debug == 1
+            trafficPktRate(win,1) = mean(datasetMtxDestIP(step:i,19));
+            VarSourceIPs(win,1) = aux/length(sourceIPs);
+            entropySourceIPs(win,1) = entropy(sourceIPs);
+            NaHidModule(win,1) = NaHid([trafficPktRate(win,1) VarSourceIPs(win,1) entropySourceIPs(win,1)],normalTraffic1);
+            NaHiDResults(win,1) = NaHidModule(win,1);
+            if(NaHidModule(win,1) < 0.81)
+                disp('traffic');
+                disp(i);
+                disp('atack');
+            else
+                disp('traffic');
+                disp(i);
+                disp('normal');
+                keyboard
+            end
+            step =  i + 1;
+            time = 0;
+            
+        else
+            VarSourceIPs = aux/length(sourceIPs);
+            entropySourceIPs = entropy(sourceIPs);
+            NaHidModule = NaHid([trafficPktRate VarSourceIPs entropySourceIPs],normalTraffic1);
+            NaHiDResults(1,win) = NaHidModule;
+            
+            if(NaHidModule < 0.81)
+                disp('traffic');
+                disp(i);
+                disp('atack');
+            else
+                disp('traffic');
+                disp(i);
+                disp('normal');
+                keyboard
+            end
+            step =  i + 1;
+            time = 0;
+        end
+    end
+    
+end
+=======
         VarSourceIPs = aux/length(sourceIPs);
         entropySourceIPs = entropy(sourceIPs);
         NaHidModule = NaHid([trafficPktRate VarSourceIPs entropySourceIPs],normalTraffic1);
@@ -68,3 +134,4 @@ end
 %         step = step + windownSize;
 %     end
 % end
+>>>>>>> 945c18613071dfe0fbd79c591b81f903cbcd4d40
