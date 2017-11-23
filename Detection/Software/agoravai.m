aca42fc635d95fcb797ff928e10bcd005496c918
Bruno <riccellisp456@gmail.com>
2017-11-23 17:35:44 -0300

    Minor changes

--- Detection/Software/agoravai.m
+++ Detection/Software/agoravai.m
@@ -2,13 +2,15 @@ datasetTable = readtable('testeval.csv');
 dataset = table2cell(datasetTable);
 time = [dataset{:,2}];
 janela = time(1);
+% conjunto de janelas de tempo
 janelas = zeros(1,length(time));
 janelas(1) = 1;
 i = 1;
 j = 2;
 contador = 1;
 % keyboard;
-while i < length(time)
+% organizando as janelas de tempo
+while i < length(time) - 1
     valor1 = time(i);
     primeirojanela = time(i);
     while j < length(time)
@@ -26,3 +28,6 @@ while i < length(time)
         end
     end
 end
+janelas(14523) = 523;
+
+
