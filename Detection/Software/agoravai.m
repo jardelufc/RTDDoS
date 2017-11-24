4ab77127c43869730e06c1495029a72e8067cf73
Riccellisp <riccellisp456@gmail.com>
2017-11-24 13:09:04 -0300

    minor changes

--- Detection/Software/agoravai.m
+++ Detection/Software/agoravai.m
@@ -1,6 +1,16 @@
 datasetTable = readtable('testeval.csv');
 dataset = table2cell(datasetTable);
 time = [dataset{:,2}];
+packetSize = [dataset{:,6}];
+numOfPackets = zeros(1,length(dataset(:,1)));
+packetRateBps = zeros(1,length(dataset(:,1)));
+sourceIPs = dataset(:,3);
+
+% Getting packet size in bps
+for k= 1:length(dataset(:,1))
+    packetRateBps(k) = packetSize(k) * 8;
+end
+
 janela = time(1);
 % conjunto de janelas de tempo
 janelas = zeros(1,length(time));
@@ -14,7 +24,7 @@ while i < length(time) - 1
     valor1 = time(i);
     primeirojanela = time(i);
     while j < length(time)
-        valor = time(j);        
+        valor = time(j);
         result = valor - primeirojanela;
         if(result <= 1.0000)
             janelas(j) = contador;
@@ -28,6 +38,20 @@ while i < length(time) - 1
         end
     end
 end
+% keyboard
 janelas(14523) = 523;
+aux = 1;
+for idx = 1:523
+    win = find(janelas == idx);
+    IPsOrigemWin = sourceIPs(win);
+    packetRate = mean(packetRateBps(win));
+    keyboard
+    for indices = 1:(length(IPsOrigemWin) - 1)
+        if IPsOrigemWin(indices)~=IPsOrigemWin(indices+1)
+            aux = aux + 1;
+        end
+    end
+    keyboard;
+end
 
 
