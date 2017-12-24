0f05d05e52f7fd0611cd32ba1e45a0d1d626ef67
Riccellisp <riccellisp456@gmail.com>
2017-12-24 02:47:35 -0300

    entropy

--- Detection/Software/entropy.m
+++ Detection/Software/entropy.m
@@ -3,7 +3,7 @@
 % detection using FPGA,2017. Section 3 
 function E = entropy(d)
 	if ischar(d), d=abs(d); end;
-        [Y,I,J] = unique(d); 	
+    [Y,I,J] = unique(d); 	
 	H = sparse(J,1,1);
 	p = full(H(H>0))/length(d);
 	E = -sum(p.*log2(p));
